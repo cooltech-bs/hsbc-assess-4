18c67afc0bac9173269c0aad3b4739361896801a
Chang Qian <cooltech.bs@gmail.com>
2022-09-20 09:11:06 +0800

    Documentize InMemoryServer

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -10,9 +10,10 @@ import (
 
 type InMemoryServerConfig struct {
 	TokenExpireSec int32
-	PruneInterval  int32
 }
 
+// InMemoryServer is an auth server that stores all its data in memory (without persistence).
+// It uses maps to provide quick access with both IDs and names as key.
 type InMemoryServer struct {
 	cfg InMemoryServerConfig
 
@@ -22,17 +23,30 @@ type InMemoryServer struct {
 	rname  map[string]*Role
 	tokens map[TokenValue]*Token
 
+	// Auto-increment numerical IDs
 	nextUser UserID
 	nextRole RoleID
 }
 
 var (
-	ErrInternal = errors.New("internal server error")
+	ErrInvalidConfig = errors.New("wrong config")
+	ErrInternal      = errors.New("internal server error")
 )
 
-func NewInMemoryServer(config InMemoryServerConfig) *InMemoryServer {
+// NewInMemoryServer creates an InMemoryServer for authentication and authorization.
+// We assume that tokens must be valid for at least 1 minute to be useful. Setting TokenExpireSec
+// to anything below that will result in an error.
+//
+// Returns: pointer to the new server instance
+// Errors: ErrInvalidConfig
+// TODO: allow setting nextUser and nextRole in config.
+func NewInMemoryServer(config *InMemoryServerConfig) (*InMemoryServer, error) {
+	if config == nil || config.TokenExpireSec < 60 {
+		return nil, ErrInvalidConfig
+	}
+
 	svr := InMemoryServer{
-		cfg:      config,
+		cfg:      *config,
 		users:    make(map[UserID]*User),
 		uname:    make(map[string]*User),
 		roles:    make(map[RoleID]*Role),
@@ -41,10 +55,15 @@ func NewInMemoryServer(config InMemoryServerConfig) *InMemoryServer {
 		nextUser: 1,
 		nextRole: 1,
 	}
-	// TODO: if cfg.PruneInterval > 0 ...
-	return &svr
+	return &svr, nil
 }
 
+// *-* Public API *-*
+
+// CreateUser adds a new user with given credentials.
+//
+// Returns: the ID of the new user
+// Errors: ErrUserExists
 func (s *InMemoryServer) CreateUser(name, password string) (UserID, error) {
 	if _, exists := s.uname[name]; exists {
 		return 0, ErrUserExists
@@ -61,6 +80,10 @@ func (s *InMemoryServer) CreateUser(name, password string) (UserID, error) {
 	return newUser.ID, nil
 }
 
+// DeleteUser removes a user with given ID.
+//
+// Returns: none
+// Errors: ErrUserNotExist
 func (s *InMemoryServer) DeleteUser(user UserID) error {
 	userObj, ok := s.users[user]
 	if !ok {
@@ -72,6 +95,10 @@ func (s *InMemoryServer) DeleteUser(user UserID) error {
 	return nil
 }
 
+// CreateRole adds a new role with given name.
+//
+// Returns: the ID of the new group
+// Errors: ErrRoleExists
 func (s *InMemoryServer) CreateRole(name string) (RoleID, error) {
 	if _, exists := s.rname[name]; exists {
 		return 0, ErrRoleExists
@@ -87,6 +114,10 @@ func (s *InMemoryServer) CreateRole(name string) (RoleID, error) {
 	return newRole.ID, nil
 }
 
+// DeleteRole removes a role with given ID.
+//
+// Returns: none
+// Errors: ErrRoleNotExist
 func (s *InMemoryServer) DeleteRole(role RoleID) error {
 	roleObj, ok := s.roles[role]
 	if !ok {
@@ -98,6 +129,11 @@ func (s *InMemoryServer) DeleteRole(role RoleID) error {
 	return nil
 }
 
+// AddRoleToUser assigns a role to a user.
+// It is a no-op if the user already has the role.
+//
+// Returns: none
+// Errors: ErrUserNotExist, ErrRoleNotExist
 func (s *InMemoryServer) AddRoleToUser(user UserID, role RoleID) error {
 	userObj, ok := s.users[user]
 	if !ok {
@@ -112,6 +148,13 @@ func (s *InMemoryServer) AddRoleToUser(user UserID, role RoleID) error {
 	return nil
 }
 
+// Authenticate checks a username/password pair, and creates a token for the user if it passes.
+// Note that the password is clear text, like that in HTTP Basic auth.
+// For security, the function does not distinguish "wrong username" from "wrong password".
+// ErrInternal is returned only in rare cases where the system cannot provide enough randomness.
+//
+// Returns: the token string
+// Errors: ErrInvalidAuth, ErrInternal
 func (s *InMemoryServer) Authenticate(username, password string) (TokenValue, error) {
 	userObj, ok := s.uname[username]
 	if !ok {
@@ -130,10 +173,17 @@ func (s *InMemoryServer) Authenticate(username, password string) (TokenValue, er
 	return token.Value, nil
 }
 
+// Invalidate invalidates a token immediately.
+//
+// Returns: none
 func (s *InMemoryServer) Invalidate(token TokenValue) {
 	delete(s.tokens, token)
 }
 
+// CheckRole checks if the user identified by the token has the given role.
+//
+// Returns: true or false
+// Errors: ErrInvalidToken
 func (s *InMemoryServer) CheckRole(token TokenValue, role RoleID) (bool, error) {
 	userObj, err := s.verifyToken(token)
 	if err != nil {
@@ -143,6 +193,10 @@ func (s *InMemoryServer) CheckRole(token TokenValue, role RoleID) (bool, error) 
 	return belongs, nil
 }
 
+// AllRoles return all role IDs associated with the user identified by the token.
+//
+// Returns: a list of RoleIDs (32-bit integers)
+// Errors: ErrInvalidToken
 func (s *InMemoryServer) AllRoles(token TokenValue) ([]RoleID, error) {
 	userObj, err := s.verifyToken(token)
 	if err != nil {
@@ -157,8 +211,32 @@ func (s *InMemoryServer) AllRoles(token TokenValue) ([]RoleID, error) {
 	return roleList, nil
 }
 
-// *-* Internal *-*
+// *-* Query operations *-*
+// These functions provide mapping between IDs and names.
+// nil is returned if the query has no result.
+// The function names are self-explanatory.
 
+func (s *InMemoryServer) GetUser(id UserID) *User {
+	return s.users[id]
+}
+
+func (s *InMemoryServer) GetUserByName(name string) *User {
+	return s.uname[name]
+}
+
+func (s *InMemoryServer) GetRole(id RoleID) *Role {
+	return s.roles[id]
+}
+
+func (s *InMemoryServer) GetRoleByName(name string) *Role {
+	return s.rname[name]
+}
+
+// *-* Internal *-*
+// Bookkeeping, including token maintenance.
+
+// newToken creates a new token for a user.
+// It optionally triggers garbage collection for expired tokens.
 func (s *InMemoryServer) newToken(u *User) (*Token, error) {
 	b := make([]byte, 8)
 	_, err := rand.Read(b)
@@ -169,11 +247,12 @@ func (s *InMemoryServer) newToken(u *User) (*Token, error) {
 	t := Token{
 		Value:   TokenValue(base64.StdEncoding.EncodeToString(b)),
 		User:    u.ID,
-		Expires: now.Add(time.Duration(s.cfg.PruneInterval) * time.Second),
+		Expires: now.Add(time.Duration(s.cfg.TokenExpireSec) * time.Second),
 	}
 	return &t, nil
 }
 
+// verifyToken ensures the token, as well as its associated user, is valid and not expired/deleted.
 func (s *InMemoryServer) verifyToken(t TokenValue) (*User, error) {
 	tokenObj, ok := s.tokens[t]
 	if !ok {
@@ -192,4 +271,11 @@ func (s *InMemoryServer) verifyToken(t TokenValue) (*User, error) {
 		return nil, ErrInvalidToken
 	}
 	return userObj, nil
+}
+
+// pruneTokens remove expired tokens from memory.
+// It is triggered roughly once per hour. TODO: support configuring this interval
+// Returns the number of tokens removed in this operation.
+func (s *InMemoryServer) pruneTokens() int {
+	//
 }
